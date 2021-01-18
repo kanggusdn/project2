4e24df613c4d82282138205f2883cc09b6c2793f
kanggusdn <gusdnkang.DE@gmail.com>
2021-01-18 13:55:22 +0900

    파일 업로드 수정

--- pj/src/action/NoticeWriteProAction.java
+++ pj/src/action/NoticeWriteProAction.java
@@ -2,6 +2,7 @@ package action;
 
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,12 +19,12 @@ public class NoticeWriteProAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ActionForward forward = null;
 		NoticeBean noticeBean = null;
-//		String realFolder = "";
-//		String saveFolder = "/boardUpload";
+		String realFolder = "";
+		String saveFolder = "/boardUpload";
 		int fileSize = 5 * 1024 * 1024;
-//		ServletContext context = request.getServletContext();
-//		realFolder = context.getRealPath(saveFolder);
-		String realFolder = "c:/boardUpload";
+		ServletContext context = request.getServletContext();
+		realFolder = context.getRealPath(saveFolder);
+//		String realFolder = "c:/boardUpload";
 		MultipartRequest multi = new MultipartRequest(request, realFolder, fileSize, "UTF-8",
 				new DefaultFileRenamePolicy());
 		noticeBean = new NoticeBean();
